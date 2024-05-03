f143f2630a74f90ec6f058a350fa2c37a3738562
PaolettoCo <paoloconti.p@gmail.com>
2024-05-03 17:53:28 +0200

    test mail

--- src/test/java/com/pasqualehorse/livecoding/controller/UserControllerTest.java
+++ src/test/java/com/pasqualehorse/livecoding/controller/UserControllerTest.java
@@ -1,8 +1,10 @@
 package com.pasqualehorse.livecoding.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.pasqualehorse.livecoding.controller.dto.BaseResponse;
 import com.pasqualehorse.livecoding.controller.dto.WithIdResponseDto;
 import com.pasqualehorse.livecoding.entity.User;
+import com.pasqualehorse.livecoding.repository.UserRepository;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,6 +12,8 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.ResultActions;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
@@ -30,9 +34,12 @@ class UserControllerTest {
     @Autowired
     ObjectMapper objectMapper;
 
+    @Autowired
+    UserRepository userRepository;
+
     @Test
     public void postUser() throws Exception {
-
+        userRepository.deleteAll();
         User user = new User("AAAAA", "AAAA@AAAA", "1234");
 
         this.mock.perform(post("/user/add")
@@ -45,5 +52,19 @@ class UserControllerTest {
                     WithIdResponseDto userOut = objectMapper.readValue(result.getResponse().getContentAsString(), WithIdResponseDto.class);
                     Assertions.assertNotNull(userOut.getId());
                 });
+    }
+    @Test
+    public void postUser_testUserNameCanNotBeDuplicate() throws Exception {
+        userRepository.deleteAll();
+        User user1 = new User("AAAAA", "AAAA@AAAA", "1234");
+        User user2 = new User("BBBBB", "AAAA@AAAA", "9874");
+        this.mock.perform(post("/user/add")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(user1))).andDo(print()).andReturn();
+        MvcResult res = this.mock.perform(post("/user/add")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(user2))).andDo(print()).andExpect(status().is(409)).andReturn();
+        BaseResponse baseResponse = objectMapper.readValue(res.getResponse().getContentAsString(), BaseResponse.class);
+        assertFalse(baseResponse.getErrorMessage().isEmpty(), "Email già presente");
     }
 }
