96c71d17ddc4d01f996b9608af174e3c47e876b8
Simon <simon.gobbato1@gmail.com>
2024-05-09 14:51:54 +0200

    User ruolo service

--- src/main/java/com/pasqualehorse/livecoding/repository/UserRuoloRepository.java
+++ src/main/java/com/pasqualehorse/livecoding/repository/UserRuoloRepository.java
@@ -1,12 +1,16 @@
 package com.pasqualehorse.livecoding.repository;
 
+import com.pasqualehorse.livecoding.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.pasqualehorse.livecoding.entity.UserRuolo;
 import com.pasqualehorse.livecoding.entity.UserRuoloPK;
 
+import java.util.List;
+
 @Repository
 public interface UserRuoloRepository extends JpaRepository<UserRuolo, UserRuoloPK>{
 
+    List<UserRuolo> findByUser(User user);
 }
--- src/main/java/com/pasqualehorse/livecoding/service/UserRuoloService.java
+++ src/main/java/com/pasqualehorse/livecoding/service/UserRuoloService.java
@@ -1,13 +1,21 @@
 package com.pasqualehorse.livecoding.service;
 
+import com.pasqualehorse.livecoding.entity.Ruolo;
+import com.pasqualehorse.livecoding.entity.User;
+import com.pasqualehorse.livecoding.entity.UserRuolo;
+import com.pasqualehorse.livecoding.repository.UserRepository;
 import org.springframework.stereotype.Service;
 
 import com.pasqualehorse.livecoding.controller.dto.UserRuoloListDTO;
 import com.pasqualehorse.livecoding.repository.UserRuoloRepository;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Service
 public class UserRuoloService {
-	
+
+	private UserRepository userRepository;
 	private UserRuoloRepository userRuoloRepository;
 	
 	public UserRuoloService(UserRuoloRepository userRuoloRepository) {
@@ -15,8 +23,22 @@ public class UserRuoloService {
 	}
 
 	public UserRuoloListDTO getAllRuoloByUser(Long id) {
-		
-		return null;
+
+		User user = userRepository.findById(id).get();
+
+		List<UserRuolo> userRuoloList = userRuoloRepository.findByUser(user);
+
+		UserRuoloListDTO userRuoloListDTO = new UserRuoloListDTO();
+		userRuoloListDTO.setUser(user);
+
+		List<Ruolo> ruolo1 = new ArrayList<>();
+		for (UserRuolo userRuolo: userRuoloList)
+		{
+			ruolo1.add(userRuolo.getRuolo());
+		}
+		userRuoloListDTO.setRuoli(ruolo1);
+
+		return userRuoloListDTO;
 	}
 	
 }
